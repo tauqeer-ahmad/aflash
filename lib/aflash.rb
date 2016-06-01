7bd75187c0ba99a3724f0a4ff56bf860aa5e9801
Tauqeer Ahmad <tauqeer.ahmad2008@gmail.com>
2016-06-01 13:19:24 +0500

    Added ajax flash files

--- lib/aflash.rb
+++ lib/aflash.rb
@@ -1,5 +1,17 @@
 require "aflash/version"
 
 module Aflash
-  # Your code goes here...
+  class Engine < ::Rails::Engine; end
+  extend ActiveSupport::Concern
+
+  included do
+    after_filter :add_flash_to_header
+  end
+
+  private
+  def add_flash_to_header
+    return unless request.xhr?
+    response.headers['X-Flash'] = flash.to_h.to_json
+    flash.discard
+  end
 end
